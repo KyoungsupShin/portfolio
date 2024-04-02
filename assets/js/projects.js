30174c1b0096d16700545f9dc496fdf5de9e0f91
Daniel <66240947+KyoungsupShin@users.noreply.github.com>
2024-04-02 20:36:44 +0000

    add

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,24 +11,33 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/image_process.png',
+            image: 'assets/images/classification.png',
             // link: 'https://github.com/abhn/Mporter',
-            title: 'Smart Factory Image processing',
+            title: 'Classification',
+            // demo: 'https://mporter.co',
+            technologies: ['Python', 'RTSP프로토콜','CentOS', 'Pytorch', 'ResNet', 'PostgreSQL', 'Spotfire'],
+            description: "실시간 이미지 프로세싱 개발 / 결과 분석 가시화 / 레시피 결과 분석",
+            categories: ['Smart Factory']
+        },
+        {
+            image: 'assets/images/objectdetection.png',
+            // link: 'https://github.com/abhn/Mporter',
+            title: 'Classification',
             // demo: 'https://mporter.co',
             technologies: ['Python', 'RTSP프로토콜','CentOS', 'Pytorch', 'YOLOv5', 'PostgreSQL', 'Spotfire'],
             description: "실시간 이미지 프로세싱 개발 / 결과 분석 가시화 / 레시피 결과 분석",
-            categories: ['image_process']
-        },
-        {
-            image: 'assets/images/mentors.jpg',
-            link: 'https://github.com/abhn/Mporter',
-            title: 'Mporter',
-            demo: 'https://mporter.co',
-            technologies: ['Flask', 'Celery', 'Python'],
-            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
-            categories: ['image_process']
+            categories: ['Smart Factory']
         },
 
+        {
+            image: 'assets/images/segmentation.png',
+            // link: 'https://github.com/abhn/Mporter',
+            title: 'Classification',
+            // demo: 'https://mporter.co',
+            technologies: ['Python', 'RTSP프로토콜','CentOS', 'Pytorch', 'Detectron', 'PostgreSQL', 'Spotfire'],
+            description: "실시간 이미지 프로세싱 개발 / 결과 분석 가시화 / 레시피 결과 분석",
+            categories: ['Smart Factory']
+        },
     ]
 
     let projects = [];
